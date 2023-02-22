2ba54c8ec7ec253b65ecfca752fb8fef9667db90
AgustinLaValla <agulavalla@gmail.com>
2023-02-22 13:38:26 -0300

    Draw Route

--- src/context/maps/MapProvider.tsx
+++ src/context/maps/MapProvider.tsx
@@ -1,4 +1,4 @@
-import { Map, Marker, Popup } from 'mapbox-gl';
+import { AnySourceData, LngLatBounds, Map, Marker, Popup } from 'mapbox-gl';
 import React from 'react'
 import { directionApi } from '../../apis';
 import { DirectionResponse } from '../../interfaces/DirectionResponse.interface';
@@ -26,14 +26,63 @@ export const MapProvider = ({ children }: MapProviderProps) => {
   const getRouteBetweenPoints = async (start: MapCoordinate, end: MapCoordinate) => {
     const { data } = await directionApi.get<DirectionResponse>(`/${start.join(',')};${end.join(',')}`);
 
-    if(data.code === 'NoRoute') alert('No route found');
+    if (data.code === 'NoRoute') alert('No route found');
 
     const { distance, duration, geometry } = data.routes[0];
+    const { coordinates } = geometry;
 
     let kms = distance / 1000;
     kms = Math.round(kms * 100) / 100;
 
     const minutes = duration / 60;
+
+    const bounds = new LngLatBounds(start, start);
+
+    coordinates.forEach(cord => {
+      const newCoord: MapCoordinate = [cord[0], cord[1]]
+      bounds.extend(newCoord)
+    });
+
+    state.map?.fitBounds(bounds, { padding: 200 });
+
+    //Polyline
+
+    const sourceData: AnySourceData = {
+      type: 'geojson',
+      data: {
+        type: 'FeatureCollection',
+        features: [
+          {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+              type: 'LineString',
+              coordinates: coordinates
+            }
+          }
+        ]
+      }
+    }
+
+    if (state.map?.getLayer('RouteString')) {
+      state.map.removeLayer('RouteString');
+      state.map.removeSource('RouteString');
+    }
+
+    state.map?.addSource('RouteString', sourceData);
+    state.map?.addLayer({
+      id: 'RouteString',
+      type: 'line',
+      source: 'RouteString',
+      layout: {
+        'line-cap': 'round',
+        'line-join': 'round'
+      },
+      paint: {
+        'line-color': '#0d6efd',
+        'line-width': 3
+      }
+    })
 
   }
 
